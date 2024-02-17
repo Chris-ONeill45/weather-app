683682df3491df80bc229803147aa16fd406a5ac
Chris O'Neill <chris_oslow@hotmail.com>
2024-02-17 14:07:30 +0000

    Location Details completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App({ location }) {
+  const { city, country } = location;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
