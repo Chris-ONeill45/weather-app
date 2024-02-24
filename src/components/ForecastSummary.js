4b81c5584e3cccfa50e5b1795e08f4011a00462f
Chris O'Neill <chris_oslow@hotmail.com>
2024-02-24 11:10:24 +0000

    Updated state and Event Handlers completed

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -2,7 +2,7 @@ import React from "react";
 import iconData from "../data/iconData.json";
 
 function ForecastSummary(props) {
-  const { date, description, icon, temperature } = props;
+  const { date, description, icon, temperature, onSelect } = props;
   const weatherCode = `${icon.slice(0, 1)}00`;
   const formattedDate = new Date(date).toDateString();
 
@@ -16,6 +16,9 @@ function ForecastSummary(props) {
         {temperature.max}&deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
+      <button type="button" onClick={() => onSelect(date)}>
+        More Details
+      </button>
     </div>
   );
 }
